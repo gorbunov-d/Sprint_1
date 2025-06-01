48a0639ff88fec9b5b5f7d7994e6c007be65fe2a
gorbunov-d <gorbunovdmitriy68@gmail.com>
2025-06-01 15:00:58 +0300

    first commit

--- Sprint_1/task_2.py
+++ Sprint_1/task_2.py
@@ -1,8 +1,6 @@
 class Tester:
     def __init__(self, name):
         self.name = name
-        self.deadline = True
-
     def work_hard(self, deadline=True):
         if deadline:
             print(self.name, 'Что ж, ещё часок поработаю!')
@@ -10,6 +8,6 @@ class Tester:
             print(self.name, 'Можно отдохнуть')
 
 tester_1 = Tester(name='tester_1')
-tester_1.work_hard(deadline=False)  # 'tester_1 Можно отдохнуть'
+tester_1.work_hard(deadline=False) 
 tester_2 = Tester(name='tester_2')
-tester_2.work_hard(deadline=True)   # 'tester_2 Что ж, ещё часок поработаю!'
+tester_2.work_hard(deadline=True)
